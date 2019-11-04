bfe8e53e47e889462df628f7eab43339baaefce5
Virsaviya <virsaviya@Stardust-21.local>
2019-11-04 12:58:18 -0800

    initial setup

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Wrapper } from './styled';
+import mock_data from '../../assets/mock_data.json';
+
+function App() {
+  return (
+    <Wrapper>
+      Hi there
+    </Wrapper>
+  );
+}
+
+export default App;
---
+++ src/components/App/styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
